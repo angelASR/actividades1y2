2573a1196d95030a89d9f33ea885136806856e84
Angel Sosa <Angel Sosa@AngelSosa-PC>
2016-05-28 13:38:14 -0400

    este es la final

--- src/cine/Funcion.java
+++ src/cine/Funcion.java
@@ -1,8 +1,9 @@
 package cine;
 
+import java.sql.Time;
+
 public class Funcion {
-//** poneme 5 profe jejeje
-	**//
+//** poneme 5 profe jejeje	**//
 	
 	
 	
@@ -13,4 +14,28 @@ public class Funcion {
 	/*
 	 * XDDDDDDD
 	 */
+	
+	private Time hora;
+
+	private Peliculas pelicula;
+	private int numeroSala;
+	public Time getHora() {
+		return hora;
+	}
+	public void setHora(Time hora) {
+		this.hora = hora;
+	}
+	public Peliculas getPelicula() {
+		return pelicula;
+	}
+	public void setPelicula(Peliculas pelicula) {
+		this.pelicula = pelicula;
+	}
+	public int getNumeroSala() {
+		return numeroSala;
+	}
+	public void setNumeroSala(int numeroSala) {
+		this.numeroSala = numeroSala;
+	}
+	
 }
